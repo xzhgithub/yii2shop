7b193fc470d8a3be134a4b9662753bf357f55cf4
xzhgithub <924670581@qq.com>
2017-07-04 14:13:01 +0800

    2

--- advanced/frontend/views/wechat/login.php
+++ advanced/frontend/views/wechat/login.php
@@ -1,5 +1,5 @@
 <form method="post">
- 用户名：<input type="text" name="username">
- 密  码：<input type="password" name="password">
+ 用户名：<input type="text" name="username"><br />
+ 密  码：<input type="password" name="password"><br />
     <input type="submit" value="提交">
 </form>
